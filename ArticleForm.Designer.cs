a8050486549712f9af4de8b20ac3fe676e084fd3
Андрей <50486086+DarkCat09@users.noreply.github.com>
2020-12-11 21:10:31 +0400

    Fixed comments and design

--- ArticleForm.Designer.cs
+++ ArticleForm.Designer.cs
@@ -49,47 +49,49 @@
             // 
             this.nameLabel.AutoEllipsis = true;
             this.nameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
-            this.nameLabel.Location = new System.Drawing.Point(15, 11);
+            this.nameLabel.Location = new System.Drawing.Point(11, 9);
+            this.nameLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.nameLabel.Name = "nameLabel";
-            this.nameLabel.Size = new System.Drawing.Size(595, 49);
+            this.nameLabel.Size = new System.Drawing.Size(611, 49);
             this.nameLabel.TabIndex = 1;
             this.nameLabel.Text = "label1";
             // 
             // textLabel
             // 
             this.textLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textLabel.Location = new System.Drawing.Point(12, 74);
-            this.textLabel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textLabel.Location = new System.Drawing.Point(9, 60);
+            this.textLabel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textLabel.Multiline = true;
             this.textLabel.Name = "textLabel";
             this.textLabel.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textLabel.Size = new System.Drawing.Size(951, 379);
+            this.textLabel.Size = new System.Drawing.Size(714, 309);
             this.textLabel.TabIndex = 10;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(60, 475);
+            this.label1.Location = new System.Drawing.Point(45, 386);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(71, 17);
+            this.label1.Size = new System.Drawing.Size(56, 13);
             this.label1.TabIndex = 6;
             this.label1.Text = "Нравится";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(188, 475);
+            this.label2.Location = new System.Drawing.Point(141, 386);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(91, 17);
+            this.label2.Size = new System.Drawing.Size(71, 13);
             this.label2.TabIndex = 7;
             this.label2.Text = "Не нравится";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(865, 463);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(649, 376);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 41);
+            this.button1.Size = new System.Drawing.Size(75, 32);
             this.button1.TabIndex = 5;
             this.button1.Text = "Закрыть";
             this.button1.UseVisualStyleBackColor = true;
@@ -98,10 +100,10 @@
             // DislikePB
             // 
             this.DislikePB.Image = global::WindowsFormsApp2.Properties.Resources.DisLikeOff;
-            this.DislikePB.Location = new System.Drawing.Point(140, 459);
-            this.DislikePB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.DislikePB.Location = new System.Drawing.Point(105, 373);
+            this.DislikePB.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.DislikePB.Name = "DislikePB";
-            this.DislikePB.Size = new System.Drawing.Size(43, 43);
+            this.DislikePB.Size = new System.Drawing.Size(32, 35);
             this.DislikePB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.DislikePB.TabIndex = 9;
             this.DislikePB.TabStop = false;
@@ -111,10 +113,10 @@
             // CommPB
             // 
             this.CommPB.Image = global::WindowsFormsApp2.Properties.Resources.Comm;
-            this.CommPB.Location = new System.Drawing.Point(288, 459);
-            this.CommPB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.CommPB.Location = new System.Drawing.Point(216, 373);
+            this.CommPB.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.CommPB.Name = "CommPB";
-            this.CommPB.Size = new System.Drawing.Size(59, 43);
+            this.CommPB.Size = new System.Drawing.Size(44, 35);
             this.CommPB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.CommPB.TabIndex = 8;
             this.CommPB.TabStop = false;
@@ -124,10 +126,10 @@
             // LikePB
             // 
             this.LikePB.Image = global::WindowsFormsApp2.Properties.Resources.LikeOff;
-            this.LikePB.Location = new System.Drawing.Point(12, 459);
-            this.LikePB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.LikePB.Location = new System.Drawing.Point(9, 373);
+            this.LikePB.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.LikePB.Name = "LikePB";
-            this.LikePB.Size = new System.Drawing.Size(43, 43);
+            this.LikePB.Size = new System.Drawing.Size(32, 35);
             this.LikePB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.LikePB.TabIndex = 4;
             this.LikePB.TabStop = false;
@@ -138,10 +140,10 @@
             // 
             this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(932, 14);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Location = new System.Drawing.Point(699, 9);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(32, 33);
+            this.pictureBox1.Size = new System.Drawing.Size(24, 27);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
@@ -149,9 +151,10 @@
             // 
             // SaveButton
             // 
-            this.SaveButton.Location = new System.Drawing.Point(611, 11);
+            this.SaveButton.Location = new System.Drawing.Point(626, 9);
+            this.SaveButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.SaveButton.Name = "SaveButton";
-            this.SaveButton.Size = new System.Drawing.Size(83, 27);
+            this.SaveButton.Size = new System.Drawing.Size(69, 27);
             this.SaveButton.TabIndex = 11;
             this.SaveButton.Text = "Сохранить";
             this.SaveButton.UseVisualStyleBackColor = true;
@@ -160,10 +163,10 @@
             // 
             // ArticleForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
-            this.ClientSize = new System.Drawing.Size(732, 419);
+            this.ClientSize = new System.Drawing.Size(732, 415);
             this.Controls.Add(this.SaveButton);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.DislikePB);
@@ -174,7 +177,7 @@
             this.Controls.Add(this.LikePB);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.nameLabel);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "ArticleForm";
             this.Text = "Статья";
             ((System.ComponentModel.ISupportInitialize)(this.DislikePB)).EndInit();
